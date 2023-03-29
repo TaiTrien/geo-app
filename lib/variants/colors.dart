f1da86269b58057d21fbc0612e3199dfd74be881
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-29 22:43:41 +0700

    feat: init colors

---
+++ lib/variants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFFFF7243);
+  static const transparent = Colors.transparent;
+  static const white = Colors.white;
+  static const black = Colors.black;
+  static const grey = Color(0xFFB6B6B6);
+}

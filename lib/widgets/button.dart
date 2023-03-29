638ecfcab65b07a3960c6330f5bded4e16bf5e60
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-29 23:18:59 +0700

    feat: create button  component

---
+++ lib/widgets/button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:geo_app/variants/colors.dart';
+
+class Button extends StatelessWidget {
+  final String label;
+  final Function onPress;
+  const Button({super.key, required this.label, required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: TextButton(
+        onPressed: () => onPress(),
+        child: Text(
+          label,
+          style: Theme.of(context).textTheme.labelMedium?.copyWith(color: AppColors.white),
+        ),
+      ),
+    );
+  }
+}

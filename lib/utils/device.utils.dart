66fc9024ecdd363c2c8488db2208136cb4bc6320
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-30 00:06:29 +0700

    feat: add device utils

---
+++ lib/utils/device.utils.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class DeviceUtils {
+  static deviceHeight(context) => MediaQuery.of(context).size.height;
+  static deviceWidth(context) => MediaQuery.of(context).size.width;
+}

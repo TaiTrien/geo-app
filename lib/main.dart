a9dc699f446ac002400d7a1f88a3891914ddacb5
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-31 01:54:36 +0700

    chore: remove init Amplify configure to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
 
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:geo_app/modules/init/init.binding.dart';
@@ -7,13 +10,26 @@ import 'package:geo_app/routes/app.pages.dart';
 import 'package:geo_app/themes/app.theme.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'amplifyconfiguration.dart';
 import 'pages/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   DartPluginRegistrant.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
+  await configureAmplify();
   runApp(const MyApp());
+}
+
+Future<void> configureAmplify() async {
+  Amplify.addPlugin(AmplifyAuthCognito());
+  try {
+    await Amplify.configure(amplifyconfig);
+  } on AmplifyAlreadyConfiguredException {
+    if (kDebugMode) {
+      print("Amplify was already configured. But having some exceptions");
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {

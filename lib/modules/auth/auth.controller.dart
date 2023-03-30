45ce43ce9117cedb64c4dc25a05def22cb1699e9
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-31 01:39:04 +0700

    feat: implement signin function

--- lib/modules/auth/auth.controller.dart
+++ lib/modules/auth/auth.controller.dart
@@ -16,17 +16,30 @@ class AuthController extends GetxController {
         CognitoUserAttributeKey.name: firstName,
         CognitoUserAttributeKey.familyName: lastName
       };
-      final result = await Amplify.Auth.signUp(
+      final SignUpResult result = await Amplify.Auth.signUp(
         username: email,
         password: password,
         options: CognitoSignUpOptions(userAttributes: userAttributes),
       );
-
-      print("hello");
-      print(result.nextStep.signUpStep);
-
       processing = false;
       if (result.nextStep.signUpStep == "CONFIRM_SIGN_UP_STEP") return true;
+      return false;
+    } on AuthException catch (e) {
+      safePrint(e.message);
+      processing = false;
+      throw Exception(e.message);
+    }
+  }
+
+  Future<bool> signin(String email, String password) async {
+    try {
+      processing = true;
+      final SignInResult result = await Amplify.Auth.signIn(
+        username: email,
+        password: password,
+      );
+      processing = false;
+      if (result.isSignedIn) return true;
       return false;
     } on AuthException catch (e) {
       safePrint(e.message);
--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
+import 'package:geo_app/modules/auth/auth.controller.dart';
 import 'package:geo_app/widgets/button.dart';
+import 'package:geo_app/widgets/page_wrapper.dart';
 import 'package:get/get.dart';
 import '../routes/app.pages.dart';
 import '../utils/device.utils.dart';
@@ -9,44 +11,48 @@ import '../variants/variants.dart';
 class Signin extends StatelessWidget {
   Signin({super.key});
   final _formKey = GlobalKey<FormState>();
+  final AuthController _authController = Get.find();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: DeviceUtils.deviceHeight(context) * 0.3,
-            width: DeviceUtils.deviceWidth(context),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(Drawables.welcomeBg),
+    return Obx(() => PageWrapper(
+          loading: _authController.processing,
+          body: Column(
+            children: [
+              Container(
+                height: DeviceUtils.deviceHeight(context) * 0.3,
+                width: DeviceUtils.deviceWidth(context),
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(Drawables.welcomeBg),
+                  ),
+                ),
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Welcome back",
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                      Text(
+                        "Log to your account",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      renderForm(context)
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              child: Column(
-                children: [
-                  Text(
-                    "Welcome back",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  Text(
-                    "Log to your account",
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  renderForm(context)
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 
   Widget renderForm(BuildContext context) {
@@ -58,6 +64,7 @@ class Signin extends StatelessWidget {
           child: Column(
             children: [
               TextFormField(
+                controller: _emailController,
                 validator: ValidationBuilder().email().required().build(),
                 decoration: const InputDecoration(
                   hintText: 'Email',
@@ -67,6 +74,7 @@ class Signin extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               TextFormField(
+                controller: _passwordController,
                 obscureText: true,
                 validator: ValidationBuilder()
                     .regExp(RegExps.password,
@@ -85,12 +93,12 @@ class Signin extends StatelessWidget {
                 margin: const EdgeInsets.symmetric(vertical: 30),
                 child: Button(
                     label: 'Sign in',
-                    onPress: () {
-                      Get.toNamed(Routes.signup);
-
+                    onPress: () async {
                       if (_formKey.currentState!.validate()) {
-                        //TODO: handle login
-                        Get.toNamed(Routes.signup);
+                        String email = _emailController.text.trim();
+                        String password = _passwordController.text.trim();
+                        bool result = await _authController.signin(email, password);
+                        if (result) Get.offAndToNamed(Routes.home);
                       }
                     }),
               ),

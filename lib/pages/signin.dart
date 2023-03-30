6768fa710c05d591c2a362c2b0178059b3bceb85
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-30 09:40:05 +0700

    feat: restructure sign in

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -1,61 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:form_validator/form_validator.dart';
 import 'package:geo_app/widgets/button.dart';
+import 'package:get/get.dart';
+import '../routes/app.pages.dart';
 import '../utils/device.utils.dart';
 import '../variants/variants.dart';
 
 class Signin extends StatelessWidget {
   Signin({super.key});
   final _formKey = GlobalKey<FormState>();
-
-  Widget renderForm(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.only(top: 30),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                validator: ValidationBuilder().email().required('Please enter your email').build(),
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                  prefixIcon: Icon(Icons.account_circle_rounded),
-                ),
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                obscureText: true,
-                validator: ValidationBuilder()
-                    .regExp(RegExp(r"/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/"),
-                        "Password contains at least 1 uppercase letter, 1 uppercase letter and 1 number or special character!")
-                    .required('Please enter your password')
-                    .build(),
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                  prefixIcon: Icon(Icons.password),
-                  suffixIcon: Icon(Icons.remove_red_eye), //TODO: handle view password
-                ),
-                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
-              ),
-              const Spacer(),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 30),
-                child: Button(
-                    label: 'Sign in',
-                    onPress: () {
-                      if (_formKey.currentState!.validate()) {
-                        //TODO: handle login
-                      }
-                    }),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,6 +45,58 @@ class Signin extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget renderForm(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: ValidationBuilder().email().required().build(),
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(Icons.account_circle_rounded),
+                ),
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                validator: ValidationBuilder()
+                    .regExp(RegExps.password,
+                        "Password contains at least 1 uppercase letter, 1 uppercase letter and 1 number or special character!")
+                    .required('Please enter your password')
+                    .build(),
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.password),
+                  suffixIcon: Icon(Icons.remove_red_eye), //TODO: handle view password
+                ),
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+              ),
+              const Spacer(),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 30),
+                child: Button(
+                    label: 'Sign in',
+                    onPress: () {
+                      Get.toNamed(Routes.signup);
+
+                      if (_formKey.currentState!.validate()) {
+                        //TODO: handle login
+                        Get.toNamed(Routes.signup);
+                      }
+                    }),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

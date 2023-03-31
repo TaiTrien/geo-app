054518ce6ff1243b2f796d6ac1f8d899219daec6
Neirt <32502320+TaiTrien@users.noreply.github.com>
2023-03-31 10:54:49 +0700

    feat: add customer address

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,13 @@
+import 'dart:ffi';
 import 'dart:math';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:geo_app/services/location.service.dart';
+import 'package:geo_app/utils/toast.utils.dart';
+import 'package:geo_app/variants/variants.dart';
 import 'package:get/get.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
@@ -15,37 +20,54 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   MapboxMapController? mapController;
+  static const LatLng _customerAddress = LatLng(-37.2561795, 143.6713972);
 
   @override
   void initState() {
     super.initState();
   }
 
-  _onMapCreated(MapboxMapController controller) {
+  void _onMapCreated(MapboxMapController controller) {
     mapController = controller;
+    navigateToCurrentLocation();
+  }
+
+  void navigateToCurrentLocation() {
     LocationServices.getCurrentLocation()
         .then((value) => {
               mapController?.animateCamera(
-                CameraUpdate.newLatLngZoom(LatLng(value.latitude, value.longitude), 17),
+                CameraUpdate.newLatLngZoom(LatLng(value.latitude, value.longitude), 10),
               )
             })
-        .catchError((error, stackTrace) {
-      log(error);
-    });
-    ;
+        .onError((error, stackTrace) => ToastUtils.showError(error.toString()));
+  }
+
+  void loadCustomerAddress(MapboxMapController controller) {
+    controller.addSymbol(const SymbolOptions(geometry: _customerAddress, iconImage: "airport-15"));
+  }
+
+  void _onStyleLoaded(MapboxMapController controller) {
+    loadCustomerAddress(controller);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MapboxMap(
-      myLocationEnabled: true,
-      myLocationTrackingMode: MyLocationTrackingMode.Tracking,
-      trackCameraPosition: true,
-      styleString: MapboxStyles.MAPBOX_STREETS,
-      accessToken: FlutterConfig.get('MAPBOX_SK'),
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: const CameraPosition(target: LatLng(0, 0)),
-    ));
+      body: MapboxMap(
+        myLocationEnabled: true,
+        myLocationTrackingMode: MyLocationTrackingMode.Tracking,
+        trackCameraPosition: true,
+        styleString: MapboxStyles.MAPBOX_STREETS,
+        onStyleLoadedCallback: () => _onStyleLoaded(mapController!),
+        accessToken: FlutterConfig.get('MAPBOX_SK'),
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(target: LatLng(0, 0)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        onPressed: navigateToCurrentLocation,
+        child: const Icon(Icons.location_searching),
+      ),
+    );
   }
 }
